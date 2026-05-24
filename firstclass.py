14adafe77a3f45b00d6285a6c74e443080e326b1
josephfeula839-wq <josephfeula839@gmail.com>
2026-05-24 19:21:14 +0530

    Initial commit

---
+++ firstclass.py
@@ -0,0 +1,8 @@
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+jose = Dog("Jose", 5)
+print(jose.name)  # Output: Jose
+print(jose.age)   # Output: 5
