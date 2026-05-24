48198b7d730d5f12f2da7baffee062692eebd356
josephfeula839-wq <josephfeula839@gmail.com>
2026-05-24 19:44:08 +0530

    vsccommit

--- firstclass.py
+++ firstclass.py
@@ -2,7 +2,16 @@ class Dog:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+    
+    def __init__(self, source):
+        self.name = source.name
+        self.age = age
 
 jose = Dog("Jose", 5)
 print(jose.name)  # Output: Jose
-print(jose.age)   # Output: 5
+print(jose.age)   # Outpu
+
+vaza = Dog(jose)
+##test comment
+print(vaza.name)  # Output: Jose
+print(vaza.age)   # Output: 5
